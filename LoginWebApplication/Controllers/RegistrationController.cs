57db1a8375600eeecb06391b2cbe3878da972ffc
Chelsey <chelsey74@live.ca>
2022-01-13 22:07:52 -0700

    Add project files.

---
+++ LoginWebApplication/Controllers/RegistrationController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Threading.Tasks;
+
+namespace LoginWebApplication.Controllers
+{
+    public class RegistrationController : ControllerBase
+    {
+
+        private readonly ILogger<RegistrationController> _logger;
+
+        public RegistrationController(ILogger<RegistrationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost, Route("api/register")]
+        public IActionResult CreateUser(Person person)
+        {
+            SaltAndHash(person);
+            return Ok(person);
+        }
+
+        public Person SaltAndHash(Person person) {
+            string password = "password";
+            byte[] salt = new byte[128 / 8];
+            using (var rngCsp = new RNGCryptoServiceProvider())
+            {
+                rngCsp.GetNonZeroBytes(salt);
+            }
+
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 100000,
+                numBytesRequested: 256 / 8));
+
+            person.Password = hashed;
+
+            return person;
+        }
+    }
+}
---
+++ LoginWebApplication/Person.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace LoginWebApplication
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+    }
+}
