caee4992d146128c04279becc4da56a74624137f
Chelsey <chelsey74@live.ca>
2022-04-17 00:18:33 -0600

    Updated script

--- LoginWebApplication/Dbscripts/createdb.sql
+++ LoginWebApplication/Dbscripts/createdb.sql
@@ -11,6 +11,23 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+
+-- Dumping database structure for buyselldb
+DROP DATABASE IF EXISTS `buyselldb`;
+CREATE DATABASE IF NOT EXISTS `buyselldb` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `buyselldb`;
+
+-- Dumping structure for table buyselldb.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `firstName` varchar(100) NOT NULL,
+  `lastName` varchar(100) NOT NULL,
+  `id` varchar(100) NOT NULL,
+  `password` varchar(1000) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  UNIQUE KEY `Unique` (`id`,`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- Data exporting was unselected.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
